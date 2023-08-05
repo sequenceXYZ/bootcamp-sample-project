e5962ab92f860d533c2670e33ff3e07707cd4065
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2023-08-05 18:12:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, this is a simple Python web application running on Docker!"
+    return "Hello, this is a simple Python web application running on Docker in EC2 instance!"
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
